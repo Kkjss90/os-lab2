d960912cd43c780c97cffb35084ceb4a5b96052b
Kkjss90 <122020808+Kkjss90@users.noreply.github.com>
2025-02-12 15:28:51 +0000

    lab2_1

--- include/bench.h
+++ include/bench.h
@@ -2,10 +2,16 @@
 #define BENCH_H
 
 #include <string>
-#include "сache.h" // Подключаем MRUCache
+#include <vector>
+
+#define CHUNK_SIZE 4096
+
+typedef std::vector<int> Chunk;
 
 void generate_file(const std::string &filename, size_t file_size_mb, int seed);
+void sort_chunk(std::vector<int> &chunk);
+void merge_sorted_chunks(const std::vector<std::string> &chunk_files, const std::string &output_file);
+void ema_sort(const std::string &input_file, const std::string &output_file, int repeat_count);
 void EmaSortInt(std::vector<int> &numbers, int repeat_count);
-void ema_sort(const std::string &input_file, const std::string &output_file, int repeat_count, MRUCache &cache);
 
 #endif // BENCH_H
